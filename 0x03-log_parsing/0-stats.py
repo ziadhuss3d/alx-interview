ce112836a219d334fdbd7a0da7660b7e9c2d74eb
ziadhuss3d <139237464+ziadhuss3d@users.noreply.github.com>
2024-10-23 19:45:51 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,14 +4,17 @@ Log parsing
 """
 import sys
 
+
 def print_metrics(file_size, status_codes):
     """
     Print metrics
     """
     print("File size: {}".format(file_size))
-    for code in sorted(status_codes.keys()):
+    codes_sorted = sorted(status_codes.keys())
+    for code in codes_sorted:
         if status_codes[code] > 0:
             print("{}: {}".format(code, status_codes[code]))
+
 
 codes_count = {'200': 0, '301': 0, '400': 0, '401': 0,
                '403': 0, '404': 0, '405': 0, '500': 0}
@@ -22,26 +25,22 @@ if __name__ == "__main__":
     try:
         for line in sys.stdin:
             try:
-                parts = line.split()
-                # Get status code and file size
-                status_code = parts[-2]
-                file_size = int(parts[-1])
-
-                # Update the counts
-                if status_code in codes_count:
+                status_code = line.split()[-2]
+                if status_code in codes_count.keys():
                     codes_count[status_code] += 1
+                # Grab file size
+                file_size = int(line.split()[-1])
                 file_size_total += file_size
             except Exception:
                 pass
-
+            # print metrics if 10 lines have been read
             count += 1
-
-            # Print metrics every 10 lines
             if count == 10:
                 print_metrics(file_size_total, codes_count)
                 count = 0
-
     except KeyboardInterrupt:
-        # Print final metrics after manual interruption
         print_metrics(file_size_total, codes_count)
-        sys.exit(0)
+        raise
+    finally:
+        # Print metrics after processing all input
+        print_metrics(file_size_total, codes_count)
