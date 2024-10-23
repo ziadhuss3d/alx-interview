c51e2fa72a73dd6ecafb2ec34bf2b38030e20831
ziadhuss3d <ziadhuss3d@example.com>
2024-10-23 16:41:13 +0000

    fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,4 +1,4 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 """
 Log parsing
 """
@@ -41,4 +41,7 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print_metrics(file_size_total, codes_count)
         raise
-   print_metrics(file_size_total, codes_count)
+    finally:
+        # Print metrics after processing all input
+        print_metrics(file_size_total, codes_count)
+
